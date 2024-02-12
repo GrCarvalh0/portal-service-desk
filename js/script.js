518e751da37fc6dcfd8a2d0b32125ee4d7264050
Guilherme RC <93487266+GrCarvalh0@users.noreply.github.com>
2024-02-12 17:40:02 +0000

    adicionado novos arquivos e pastas

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('pass').value;
 
-    if (login == "grcarvalho" && senha == "123"){
-        alert ('Acesso permitido');
-        location.href = "home.html";
+    if (login == "sdmarketdata" && senha == "M@rketdata@sd24"){
+        location.href = "../html/home.html";
     }else{
         alert('Acesso negado');
     }
